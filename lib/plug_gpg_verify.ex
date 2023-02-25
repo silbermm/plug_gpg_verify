bdb0231e96e33c3e926deb86596840a4e8fcc5ea
Matt <matt@silbernagel.dev>
2023-02-24 21:39:00 -0500

    Initial commit

---
+++ lib/plug_gpg_verify.ex
@@ -0,0 +1,18 @@
+defmodule PlugGpgVerify do
+  @moduledoc """
+  Documentation for `PlugGpgVerify`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugGpgVerify.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_gpg_verify_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugGpgVerifyTest do
+  use ExUnit.Case
+  doctest PlugGpgVerify
+
+  test "greets the world" do
+    assert PlugGpgVerify.hello() == :world
+  end
+end
